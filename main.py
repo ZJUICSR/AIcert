c916b58fb3a452e712a5e1fc4107ac699b1bfdff
chunlai1 <1017313484@qq.com>
2022-03-17 10:59:21 +0800

    commit

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm123456')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
