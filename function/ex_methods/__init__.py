e2a1be0ad0aba52f2e737cd4e1ffc30de617a830
liuyuchen <295886980@qq.com>
2023-03-02 07:06:14 +0000

    对抗攻击机理热力图功能提交

---
+++ function/ex_methods/__init__.py
@@ -0,0 +1,315 @@
+import os.path as osp
+import numpy as np
+import torch
+import torchvision
+from PIL import Image
+
+from ex_methods.module.load_model import load_model
+from ex_methods.module.func import grad_visualize, lrp_visualize, target_layer
+from ex_methods.module.layer_activation_with_guided_backprop import get_all_layer_analysis
+from ex_methods.module.func import get_class_list, get_target_num, grad_visualize, convert_to_grayscale, \
+    loader2imagelist, save_ex_img, save_process_result, get_normalize_para
+from scipy.stats import kendalltau
+
+from .lime import lime_image
+from skimage.color import rgb2gray
+from skimage.segmentation import mark_boundaries
+from ex_methods.module.func import preprocess_transform
+
+'''预测函数'''
+
+
+def predict(net, x):
+    activation_output = net.forward(x)
+    _, prediction = torch.max(activation_output, 1)
+    return prediction, activation_output
+
+
+'''获取基于梯度/反向传播的解释方法'''
+
+
+def get_gradbase_explain(img_x, net, target_layer, dataset, ex_methods, device, class_list):
+    prediction, activation_output = predict(net, img_x)
+    class_name = [class_list[i] for i in prediction]
+
+    lrp_list, gradcam_list, ig_list = [], [], []
+    # 和pytorch自动求导计算得到的梯度值基本一致，但存在浮点后几位细微区别
+    for method in ex_methods:
+        if method == "lrp":
+            lrp_list = explain_lrp(net, img_x, dataset,
+                                   device, prediction, activation_output)
+        elif method == "gradcam":
+            gradcam_list = explain_gradcam(
+                net, img_x, target_layer, dataset, device, prediction, activation_output)
+        elif method == "integrated_grad":
+            ig_list = explain_ig(net, img_x, dataset, device,
+                                 prediction, activation_output)
+
+    return lrp_list, gradcam_list, ig_list, class_name
+
+
+'''lrp解释算法接口'''
+
+
+def explain_lrp(net, x, dataset, device, prediction, activation_output):
+    result_lrp = net.interpretation(activation_output,
+                                    interpreter="lrp",
+                                    labels=prediction,
+                                    num_target=get_target_num(dataset),
+                                    device=device,
+                                    target_layer=None,
+                                    inputs=x)
+    imgs_lrp = lrp_visualize(result_lrp, 0.9)
+    img_list = []
+    for img in imgs_lrp:
+        img_list.append(Image.fromarray((img * 255).astype(np.uint8)))
+    return img_list
+
+
+'''grad-cam解释算法接口'''
+
+
+def explain_gradcam(net, x, target_layer, dataset, device, prediction, activation_output):
+    result_cam = net.interpretation(activation_output,
+                                    interpreter="grad_cam",
+                                    labels=prediction,
+                                    num_target=get_target_num(dataset),
+                                    device=device,
+                                    target_layer=target_layer,
+                                    inputs=x)
+    x = x.permute(0, 2, 3, 1).cpu().detach().numpy()
+    x = x - x.min(axis=(1, 2, 3), keepdims=True)
+    x = x / x.max(axis=(1, 2, 3), keepdims=True)
+    imgs_gradcam = grad_visualize(result_cam, x)
+    img_list = []
+    for img in imgs_gradcam:
+        img_list.append(Image.fromarray((img * 255).astype(np.uint8)))
+    return img_list
+
+
+'''积分梯度解释算法接口'''
+
+
+def explain_ig(net, x, dataset, device, prediction, activation_output):
+    result_ig = net.interpretation(activation_output,
+                                   interpreter="integrated_grad",
+                                   labels=prediction,
+                                   num_target=get_target_num(dataset),
+                                   device=device,
+                                   target_layer=None,
+                                   inputs=x)
+    img_list = []
+    for index in range(len(result_ig)):
+        # Convert to grayscale
+        result = result_ig[index].cpu().detach().numpy()
+        grayscale_integrated_grad = convert_to_grayscale(
+            result)[0]  # [0] to remove the first dim
+        img_list.append(Image.fromarray(
+            (grayscale_integrated_grad * 255).astype(np.uint8)))
+    return img_list
+
+
+'''课题一总接口'''
+
+
+def run(model, nor_loader, adv_dataloader, ex_methods, params, size, logging):
+    """
+    :param model: pytorch模型类型
+    :param test_loader: 正常样本测试集
+    :param adv_dataloader: 对抗样本测试集: type:dict
+    :param params: 其他参数
+    :return: 总接口
+    """
+    dataset = params["dataset"]["name"].lower()
+    model_name = params["model"]["name"].lower()
+    # model_name = "resnet34"
+
+    save_path = params["out_path"]
+    root = params["root"]
+    results = {"nor": {}, "adv_methods": []}
+    device = params["device"]
+
+    mean, std = get_normalize_para(dataset)
+    trans = torchvision.transforms.Compose([
+        torchvision.transforms.Normalize(
+            mean=mean, std=std)
+    ])
+
+    class_list = get_class_list(dataset, root)
+    logging.info("[加载被解释模型]:准备加载{:s}模型".format(model_name))
+    net = load_model(model_name, pretrained=True,
+                     reference_model=model, num_classes=get_target_num(dataset))
+    net = net.eval().to(device)
+    logging.info("[加载被解释模型]:{:s}模型已加载完成".format(model_name))
+
+    t_layer = target_layer(model_name, dataset)
+    nor_img_list = loader2imagelist(nor_loader, dataset, size)
+
+    logging.info("[执行解释算法]:系统已准备完成，开始运行所选解释算法")
+    logging.info("[正常样本分析阶段]：正在对正常样本进行分析")
+    lrp_result_list, gradcam_result_list, ig_result_list, class_name_list = [], [], [], []
+    for imgs_x, label in nor_loader:
+        imgs_x = trans(imgs_x)
+        label = label.to(device)
+        imgs_x = imgs_x.to(device)
+        lrp_list, gradcam_list, ig_list, class_name = get_gradbase_explain(
+            imgs_x, net, t_layer, dataset, ex_methods, device, class_list)
+        lrp_result_list.extend(lrp_list)
+        gradcam_result_list.extend(gradcam_list)
+        ig_result_list.extend(ig_list)
+        class_name_list.extend(class_name)
+    nor_ex_dict = {"lrp": lrp_result_list,
+                   "gradcam": gradcam_result_list,
+                   "ig": ig_result_list}
+    logging.info("[正常样本分析阶段]：正常样本分析阶段已完成")
+
+    # 存储指定size数量的正常图片
+    nor_f = []
+    for i in range(size):
+        nor_f.append(save_ex_img(save_path, nor_img_list[i], f"nor_{i}.png"))
+    for ex_name, nor_ex in nor_ex_dict.items():
+        nor_ex_f = []
+        if len(nor_ex) == 0:
+            continue
+        for i in range(size):
+            nor_ex_f.append(save_ex_img(
+                save_path, nor_ex[i], f"nor_{ex_name}_{i}.png"))
+        results["nor"].update({
+            f'{ex_name}': nor_ex_f
+        })
+    results["nor"].update({
+        "nor_imgs": nor_f,
+        "class_name": class_name_list[:size]
+    })
+
+    if len(adv_dataloader) != 0:
+        adv_ex_dict = {}
+        for method, adv_loader in adv_dataloader.items():
+            logging.info("[对抗样本分析阶段]：当前执行:{:s}对抗样本攻击机理分析".format(str(method)))
+            results["adv_methods"].append(method)
+            adv_img_list = loader2imagelist(adv_loader, dataset, size)
+            lrp_result_list, gradcam_result_list, ig_result_list, class_name_list = [], [], [], []
+            for imgs_x, label in adv_loader:
+                imgs_x = imgs_x.to(device)
+                label = label.to(device)
+                lrp_list, gradcam_list, ig_list, class_name = get_gradbase_explain(
+                    imgs_x, net, t_layer, dataset, ex_methods, device, class_list)
+                lrp_result_list.extend(lrp_list)
+                gradcam_result_list.extend(gradcam_list)
+                ig_result_list.extend(ig_list)
+                class_name_list.extend(class_name)
+                results[method] = {}
+            adv_ex_dict[method] = {"lrp": lrp_result_list,
+                                   "gradcam": gradcam_result_list, "ig": ig_result_list}
+
+            # 存储指定size数量的对抗样本解释图
+            adv_f = []
+            for i in range(size):
+                adv_f.append(save_ex_img(
+                    save_path, adv_img_list[i], f"{method}_adv_{i}.png"))
+            for ex_name, adv_ex in adv_ex_dict[method].items():
+                adv_ex_f = []
+                if len(adv_ex) == 0:
+                    continue
+                for i in range(size):
+                    adv_ex_f.append(save_ex_img(
+                        save_path, adv_ex[i], f"{method}_adv_{ex_name}_{i}.png"))
+                results[method].update({
+                    f"{ex_name}": adv_ex_f
+                })
+            results[method].update({
+                "adv_imgs": adv_f,
+                "class_name": class_name_list[:size]
+            })
+        logging.info("[解释结果量化分析阶段]：采用肯德尔相关系数对解释结果进行分析")
+        kendalltau_result = get_kendalltau(nor_ex_dict, adv_ex_dict, save_path)
+        results["kendalltau"] = kendalltau_result
+        logging.info("[解释结果量化分析阶段]：解释结果量化分析已完成")
+
+    save_process_result(save_path, results, filename="keti1.json")
+    return results
+
+
+'''获取模型每层内的特征并做可视化'''
+
+
+def layer_explain(model_name, loader, adv_loader, params):
+    dataset = params["ex_methods"]["dataset"].lower()
+    save_path = osp.join(params["out_path"], "layer_explain")
+    result = get_all_layer_analysis(
+        model_name=model_name, nor_loader=loader, adv_loader=adv_loader, dataset=dataset, save_path=save_path)
+    save_process_result(root=save_path, results=result,
+                        filename="layer_vis_result.json")
+
+
+"""计算正常样本和对抗样本解释图的肯德尔相关系数"""
+
+
+def get_kendalltau(nor_dict, adv_dict, save_path):
+    ex_methods = list(nor_dict.keys())
+    adv_methods = list(adv_dict.keys())
+    save_path = osp.join(save_path, 'kendalltau')
+    result = {}
+    for adv_method in adv_methods:
+        values = {}
+        for nor_ex, adv_ex, method in zip(nor_dict.values(), adv_dict[adv_method].values(), ex_methods):
+            tmp = []
+            for nor_ex_img, adv_ex_img in zip(nor_ex, adv_ex):
+                value, _ = kendalltau(np.array(nor_ex_img, dtype="float"), np.array(
+                    adv_ex_img, dtype="float"))
+                tmp.append(value)
+            values[f"{method}"] = tmp
+        result[f"{adv_method}"] = values
+    return result
+
+
+def batch_predict(images, model, device, dataset):
+    """
+    lime 中对随机取样图像进行预测
+    :param images: np.array
+    :param model:
+    :param device:
+    :return:
+    """
+    if dataset == "mnist":
+        images = rgb2gray(images)
+    batch = torch.stack(tuple(preprocess_transform(i, dataset)
+                        for i in images), dim=0)
+    batch = batch.to(device).type(dtype=torch.float32)
+    probs = model.forward(batch)
+    return probs.detach().cpu().numpy()
+
+
+def draw_lime(img, net, device, dataset):
+    explainer = lime_image.LimeImageExplainer()
+    explanation = explainer.explain_instance(np.array(img),
+                                             net,
+                                             device,
+                                             dataset,
+                                             batch_predict,  # classification function
+                                             top_labels=5,
+                                             hide_color=0,
+                                             num_samples=1000)  # number of images that will be sent to classification function
+
+    temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, negative_only=False,
+                                                num_features=5, hide_rest=False)
+    img_boundry = mark_boundaries(temp / 255.0, mask)
+    img_boundry = Image.fromarray((img_boundry * 255).astype(np.uint8))
+    return img_boundry
+
+
+def lime_image_ex(img, net, dataset, device, model):
+    class_list = get_class_list(dataset)
+    ex_images = {}
+    i = 0
+    for img in imgs:
+        if dataset == "mnist":
+            img = img.convert("L")
+        x = load_image(device, img, dataset)
+        prediction, activation_output = predict(x, net, device)
+        class_name = class_list[prediction.item()]
+        img_lime = draw_lime(img, net, device, dataset)
+        ex_images[f"image_{i}"] = {"class_name": class_name,
+                                   "ex_imgs": [img_l, img_h, img_lime]}
+        i += 1
+    return ex_images
---
+++ function/ex_methods/module/layer_activation_with_guided_backprop.py
@@ -0,0 +1,203 @@
+"""
+Created on Thu Oct 26 11:23:47 2017
+@author: Utku Ozbulak - github.com/utkuozbulak
+"""
+import imp
+import os
+import os.path as osp
+import json
+import torch
+from torch.utils.data import dataset
+import numpy as np
+import torchvision
+from torch.autograd import Variable
+from torch.nn import ReLU
+
+from ex_methods.module.func import save_gradient_images, load_image,loader2imagelist
+from scipy.stats import kendalltau
+
+class GuidedBackprop():
+    """
+       Produces gradients generated with guided back propagation from the given image
+    """
+
+    def __init__(self, model):
+        self.model = self.process_model(model)
+        self.gradients = None
+        self.forward_relu_outputs = []
+        # Put model in evaluation mode
+        self.model.eval()
+        self.update_relus()
+        self.hook_layers()
+
+    def hook_layers(self):
+        def hook_function(module, grad_in, grad_out):
+            self.gradients = grad_in[0]
+        # Register hook to the first layer
+        first_layer = list(self.model.features._modules.items())[0][1]
+        first_layer.register_full_backward_hook(hook_function)
+
+    def process_model(self, model):
+        #set torch.nn.ReLU inplace to be False
+        for name, module in model._modules.items():
+            if isinstance(module, torch.nn.Sequential):
+                for m in module:
+                    if isinstance(m,torch.nn.ReLU):
+                        m.inplace = False
+            else:
+                if isinstance(module, torch.nn.ReLU):
+                    module.inplace = False
+        return model
+
+    def update_relus(self):
+        """
+            Updates relu activation functions so that
+                1- stores output in forward pass
+                2- imputes zero for gradient values that are less than zero
+        """
+        def relu_backward_hook_function(module, grad_in, grad_out):
+            """
+            If there is a negative gradient, change it to zero
+            """
+            # Get last forward output
+            corresponding_forward_output = self.forward_relu_outputs[-1]
+            corresponding_forward_output[corresponding_forward_output > 0] = 1
+            modified_grad_out = corresponding_forward_output * \
+                torch.clamp(grad_in[0], min=0.0)
+            del self.forward_relu_outputs[-1]  # Remove last forward output
+            return (modified_grad_out,)
+
+        def relu_forward_hook_function(module, ten_in, ten_out):
+            """
+            Store results of forward pass
+            """
+            self.forward_relu_outputs.append(ten_out)
+
+        # Loop through layers, hook up ReLUs
+        for pos, module in self.model.features._modules.items():
+            if isinstance(module, ReLU):
+                module.register_full_backward_hook(relu_backward_hook_function)
+                module.register_forward_hook(relu_forward_hook_function)
+
+    def get_maxvalue_filter(self, x):
+        x = torch.sum(x, dim=(2,3))
+        index = torch.max(x,1)[1]
+        return index
+
+    def generate_gradients(self, input_image, cnn_layer):
+        self.model.zero_grad()
+        # Forward pass
+        x = input_image
+        for index, layer in enumerate(self.model.features):
+            # Forward pass layer by layer
+            # x is not used after this point because it is only needed to trigger
+            # the forward hook function
+            x = layer(x)
+            # Only need to forward until the selected layer is reached
+            if index == cnn_layer:
+                # (forward hook function triggered)
+                break
+        filter_pos = self.get_maxvalue_filter(x)
+        conv_output = torch.sum(torch.abs(x[0, filter_pos]))
+        # Backward pass
+        conv_output.backward()
+        # Convert Pytorch variable to numpy array
+        # [0] to get rid of the first channel (1,3,224,224)
+        gradients_as_arr = self.gradients.data.numpy()[0]
+        return gradients_as_arr
+
+def get_model_and_layers(model_name):
+    cnn_layers = None
+    model = None
+    
+    if model_name == "vgg19":
+        model = torchvision.models.vgg19(weights=torchvision.models.VGG19_Weights.IMAGENET1K_V1)
+        cnn_layers = [1, 3, 6, 8, 11, 13, 15, 17, 20, 22, 24, 26, 29, 31, 33, 35]
+    elif model_name == "alexnet":
+        model = torchvision.models.alexnet(weights=torchvision.models.AlexNet_Weights.IMAGENET1K_V1)
+        cnn_layers = [1, 4, 7, 9, 11]
+    return model, cnn_layers
+
+def layer_analysis(imgs_list, file_name_to_export, model_name, save_path):
+    model, cnn_layers = get_model_and_layers(model_name)
+    result = {}
+    nor_ex_list = []
+    adv_ex_list = []
+    save_path = osp.join(save_path, model_name)
+    # Guided backprop
+    GBP = GuidedBackprop(model)
+    for name in file_name_to_export:
+        i = 0
+        for prep_img in imgs_list.get(name):
+            _prep_img = Variable(prep_img, requires_grad=True)
+            # creat a folder
+            if not os.path.exists(save_path+'/img_' + f"{i}/" + name):
+                os.makedirs(save_path+'/img_'+f"{i}/" + name)
+            if result.get(f'img_{i}') == None:
+                result[f'img_{i}'] = {}
+            if result.get("value") == None:
+                result["value"] = {}
+            tmp_list = []
+            # [0, 2, 5, 7, 10, 12, 14, 16, 19, 21, 23, 25, 28, 30, 32, 34]
+            for cnn_layer in cnn_layers:
+                # File export name
+                filename = save_path + '/img_' + \
+                    str(i) + "/" + name + \
+                    "/layer" + str(cnn_layer) + ".jpg"
+                print(filename)
+                # Get gradients
+                guided_grads = GBP.generate_gradients(
+                    _prep_img, cnn_layer)
+                # Save colored gradients
+                path = save_gradient_images(guided_grads, filename)
+                tmp_list.append(guided_grads)
+                result[f'img_{i}'].update({
+                    f"{name}{cnn_layer}": path
+                })
+                # Convert to grayscale
+                # grayscale_guided_grads = convert_to_grayscale(guided_grads)
+                # Save grayscale gradients
+                # save_gradient_images(grayscale_guided_grads, filename + '_Guided_BP_gray')
+                # Positive and negative saliency maps
+                # pos_sal, neg_sal = get_positive_negative_saliency(guided_grads)
+                # save_gradient_images(pos_sal, filename + '_pos_sal')
+                # save_gradient_images(neg_sal, filename + '_neg_sal')
+            if name == "nor":
+                nor_ex_list.append(tmp_list)
+            else: 
+                adv_ex_list.append(tmp_list)
+            i = i + 1
+    j = 0
+    for nor_imgs, adv_imgs in zip(nor_ex_list,adv_ex_list):
+        t_list = []
+        for nor_layer, adv_layer in zip(nor_imgs,adv_imgs):    
+            kendall_value, _ = kendalltau(np.array(nor_layer,dtype="float64"),np.array(adv_layer,dtype="float64"))
+            t_list.append(kendall_value)
+        result["value"].update({
+            f"img_{j}": t_list
+        })
+        j = j + 1
+    print('Layer Guided backprop completed')
+    return result
+
+
+def get_all_layer_analysis(model_name, nor_loader, adv_loader, dataset, save_path):
+    device = torch.device("cpu")
+    nor_tensor_imgs = []
+    adv_tensor_imgs = []
+
+    # model = model.to(device)
+    file_to_export = ["nor", "adv"]
+    loader = nor_loader
+    nor_img_list = loader2imagelist(loader, dataset, size=5)
+    for img in nor_img_list:
+        nor_tensor_img = load_image(device, img, dataset)
+        nor_tensor_imgs.append(nor_tensor_img)
+    adv_loader = adv_loader
+    adv_img_list = loader2imagelist(adv_loader, dataset,size=5)
+    for img in adv_img_list:
+        adv_tensor_img = load_image(device, img, dataset)
+        adv_tensor_imgs.append(adv_tensor_img)
+    list = {"nor": nor_tensor_imgs, "adv": adv_tensor_imgs}
+    result = layer_analysis(list, file_to_export, model_name, save_path)
+    return result
