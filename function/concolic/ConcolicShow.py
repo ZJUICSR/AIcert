dca401caac9a27d911fcb130148aa796088c7359
NiuWenjie <winnie_niuwj@163.com>
2023-03-07 02:28:23 +0000

    参照工程规范

--- function/concolic/ConcolicShow.py
+++ function/concolic/ConcolicShow.py
@@ -337,7 +337,7 @@ class MNIST(object):
         test_iter = Data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
         net = LeNet()
         # net.load_state_dict(torch.load(basepath + '/Utils/Models/MNIST_lenet.pth'))
-        net.load_state_dict(torch.load(basepath[:-18] + '/model/mnist/MNIST_lenet.pth'))
+        net.load_state_dict(torch.load(basepath[:-18] + '/model/ckpt/MNIST_lenet.pth'))
         return net, test_iter
 
     def ReNormalization(inputs):
@@ -368,7 +368,7 @@ class Cifar10(object):
         test_iter = Data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
         net = VGG16_torch()
         # net.load_state_dict(torch.load(basepath + '/Utils/Models/cifar10_vgg16.pth'))
-        net.load_state_dict(torch.load(basepath[:-18] + '/model/cifar/cifar10_vgg16.pth'))
+        net.load_state_dict(torch.load(basepath[:-18] + '/model/ckpt/cifar10_vgg16.pth'))
         return net, test_iter
 
     def ReNormalization(inputs):
