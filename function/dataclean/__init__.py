8fd1158d4a605ef804a85bd7eeadef74e3540a8a
NiuWenjie <winnie_niuwj@163.com>
2023-04-07 10:41:01 +0800

    opensourcev1.0 functions debug

--- function/dataclean/__init__.py
+++ function/dataclean/__init__.py
@@ -16,27 +16,33 @@ sys.path.append(ROOT)
 
 def run_dataclean(dataset):
     if dataset == "CIFAR10":
-        print()
+        print("here!")
         transform = transforms.ToTensor()
-        train_data = datasets.CIFAR10(root=ROOT[:-19]+"/dataset/data",train=True,download=True,transform=transform)
-        test_data = datasets.CIFAR10(root=ROOT[:-19]+"/dataset/data",train=False,download=True,transform=transform)
+        train_data = datasets.CIFAR10(root="./dataset/data",train=True,download=True,transform=transform)
+        test_data = datasets.CIFAR10(root="./dataset/data",train=False,download=True,transform=transform)
         train_loader = DataLoader(train_data,batch_size=4)
         test_loader = DataLoader(test_data,batch_size=4)
-        run_cleanlab(train_loader, test_loader,root=ROOT, dataset=dataset,batch_size=test_loader.batch_size, PERT_NUM=100, MAX_IMAGES=32, log_func=print)
+        res = run_cleanlab(train_loader, test_loader,root=ROOT, dataset=dataset,batch_size=test_loader.batch_size, PERT_NUM=100, MAX_IMAGES=32, log_func=print)
+        return res
     elif dataset=="MNIST":
         # run_cleanlab参数需确认
         transform = transforms.ToTensor()
-        train_data = datasets.MNIST(root=ROOT[:-19]+"/dataset/data",train=True,download=True,transform=transform)
-        test_data = datasets.MNIST(root=ROOT[:-19]+"/dataset/data",train=False,download=True,transform=transform)
+        train_data = datasets.MNIST(root="./dataset/data",train=True,download=True,transform=transform)
+        test_data = datasets.MNIST(root="/dataset/data",train=False,download=True,transform=transform)
         train_loader = DataLoader(train_data,batch_size=4)
         test_loader = DataLoader(test_data,batch_size=4)
-        run_cleanlab(train_loader, test_loader,root=ROOT, dataset=dataset,batch_size=test_loader.batch_size, PERT_NUM=100, MAX_IMAGES=32, log_func=print)
-    elif dataset=="Text": 
+        res = run_cleanlab(train_loader, test_loader,root=ROOT, dataset=dataset,batch_size=test_loader.batch_size, PERT_NUM=100, MAX_IMAGES=32, log_func=print)
+        return res
+    elif dataset=="THUCNews": 
         run_format_clean(inputfile=osp.join(current_dir,'text_sample1.txt'),outputfile=osp.join(ROOT,'text_sample1_benign.txt'),filler=" ",root=ROOT)
         run_encoding_clean(inputfile=osp.join(current_dir,'text_sample2.txt'),outputfile=osp.join(ROOT,'text_sample2_benign.txt'),root=ROOT)
-    elif dataset=="Table":
+        # res = 
+        # return res
+    elif dataset=="demo.npz":
         generate_abnormal_sample(outputfile=osp.join(current_dir,'abnormal_table.npz'))
         run_abnormal_table(inputfile=osp.join(current_dir,'abnormal_table.npz'),outputfile=osp.join(ROOT,'benign_table.npy'),root=ROOT)
+        # res = 
+        # return res
     else:
         # 上传
         pass
