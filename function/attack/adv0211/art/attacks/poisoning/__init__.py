13b87a126c606d1234d795e2c75337e583e8c3eb
chunlai <1017313484@qq.com>
2023-04-22 13:53:29 +0800

    修改对抗攻击

--- function/attack/adv0211/art/attacks/poisoning/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module providing poisoning attacks under a common interface.
-"""
-from art.attacks.poisoning.backdoor_attack import PoisoningAttackBackdoor
-from art.attacks.poisoning.feature_collision_attack import FeatureCollisionAttack
-from art.attacks.poisoning.adversarial_embedding_attack import PoisoningAttackAdversarialEmbedding
-from art.attacks.poisoning.clean_label_backdoor_attack import PoisoningAttackCleanLabelBackdoor
--- function/attack/adv0211/art/defences/detector/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Module implementing detector-based defences against adversarial attacks.
-"""
-from art.defences.detector import evasion
-from art.defences.detector import poison
