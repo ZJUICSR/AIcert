bd8365237415bfb01be76e5398f6854a01a0fb21
signedqiu <10063108+signedqiu@user.noreply.gitee.com>
2023-04-24 18:12:07 +0800

    fix the preprocessor bug

--- function/attack/estimators/object_detection/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Module containing estimators for object detection.
-"""
-from function.attack.estimators.object_detection.object_detector import ObjectDetectorMixin
-
-from function.attack.estimators.object_detection.pytorch_object_detector import PyTorchObjectDetector
-from function.attack.estimators.object_detection.pytorch_faster_rcnn import PyTorchFasterRCNN
-from function.attack.estimators.object_detection.pytorch_yolo import PyTorchYolo
-from function.attack.estimators.object_detection.tensorflow_faster_rcnn import TensorFlowFasterRCNN
