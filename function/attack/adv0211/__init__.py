f9fb2827d30cd9375374b01864262a32b67bd879
chunlai <1017313484@qq.com>
2023-04-24 18:42:38 +0800

    修改bug

--- function/attack/adv0211/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.join(os.getcwd(),"function/attack/adv0211"))
-print(os.path.join(os.getcwd(),"function/attack/adv0211"))
-from attack_api import EvasionAttacker, BackdoorAttacker
-
-from art.mnist import Mnist
