ab6524f655ad8b6da68dd6b0ab5612314693a2a7
krumper <pengkuang.cn@gmail.com>
2023-04-28 22:00:34 +0800

    bug fix: negative proportion, update fairness metrics equal opportunity

--- function/fairness/model_metrics.py
+++ function/fairness/model_metrics.py
@@ -11,31 +11,69 @@ from metrics.metric_utils import *
 #     return result
 
 # select out priviledged and unpriiviledged samples from dataset
+METRICS_FULL_NAME = {
+        # label-agnostic group fairness metrics
+        "DI" : 'Dsiaprate Impact',
+        "DP": 'Demographic Parity',
+        # single group evaluation metric(with out sensitive attribute z)
+        "OMd": 'Overall Misclassification Difference',
+        "OMr": 'Overall Misclassification Ratio',
+        "FPd": 'False Positive Difference',
+        "FPr": 'False Positive Ratio',
+        "TPd": 'True Positive Difference',
+        "TPr": 'True Positive Ratio',
+        "FNd": 'False Negative Difference',
+        "FNr": 'False Negative Ratio',
+        "TNd": 'True Negative Difference',
+        "TNr": 'True Negative Ratio',
+        "FOd": 'False Omission Difference',
+        "FOr": 'False Omission Ratio',
+        "FDd": 'False Discovery Difference',
+        "FDr": 'False Discovery Ratio',
+        # prediction fairness metrics
+        "PE": 'Predictive Equality',
+        "EOP": 'Equal Opportunity',
+        "EOD": 'Equal Odds',
+        "PP": 'Predictive Parity',
+    }
+
 class ModelMetrics(DatasetMetrics): # binary label and binary group only
 
     FAIRNESS_METRICS={
         # label-agnostic group fairness metrics
         "DI" : disparate_impact,
         "DP": demographic_parity,
+        "DP_norm": normalized_DP,
+
         # single group evaluation metric(with out sensitive attribute z)
         "OMd": overall_misc,
         "OMr": overall_misc,
+        "FPn": false_positive_num,
         "FPd": false_positive,
         "FPr": false_positive,
+        "TPn": true_positive_num,
         "TPd": true_positive,
         "TPr": true_positive,
+        "FNn": false_negtive_num,
         "FNd": false_negtive,
         "FNr": false_negtive,
+        "TNn": true_negtive_num,
         "TNd": true_negtive,
         "TNr": true_negtive,
         "FOd": false_omission,
         "FOr": false_omission,
         "FDd": false_discovery,
         "FDr": false_discovery,
+        "PRd": precision,
+        "F1d": F1_score,
+
         # prediction fairness metrics
         "PE": predictive_equality,
+        "EOP": equal_opportunity,
         "EOD": equal_odds,
         "PP": predictive_parity,
+        "TEE": treatment_equality,
+        "PED": predictive_difference,
     }
 
     BASE_METRICS={
@@ -48,13 +86,12 @@ class ModelMetrics(DatasetMetrics): # binary label and binary group only
     }
 
     # sensitive attributes and label are given by dataset
-    def __init__(self, dataset: FairnessDataset, classified_dataset: FairnessDataset, thresh=0.5, sensitive=[]):
+    def __init__(self, dataset: FairnessDataset, classified_dataset: FairnessDataset, sensitive=[]):
         super().__init__(dataset=dataset, sensitive=sensitive)
         # self.dataset = dataset
         self.classifed_dataset = classified_dataset
         # self.priviledged = dataset.privileged
         # self.favorable = dataset.favorable
-        self.thresh = thresh
 
         # extract vector of sensitive attribute and label
         # self.z, self.y = np.array(dataset.Z), np.array(dataset.Y)
@@ -72,25 +109,33 @@ class ModelMetrics(DatasetMetrics): # binary label and binary group only
         # if len(self.sensitive) > 1:
         #     self.sensitive = self.sensitive[:1] # only a single sensitive attribute supported
 
-    def group_fairness_metrics(self, metrics="DP"):
+    def group_fairness_metrics(self, metrics="DP", thd=0.5):
         if metrics not in list(ModelMetrics.FAIRNESS_METRICS.keys()):
             raise ValueError(f"Metrices \'{metrics}\' not supported.")
         func = ModelMetrics.FAIRNESS_METRICS[metrics]
-        if metrics.endswith('d'):
-            return self.diff(func)
+        if metrics.endswith('d') or metrics.endswith('n'):
+            return self.diff(func, thresh=thd)
         elif metrics.endswith('r'):
             return self.ratio(func)
         else:
-            return self.base_rate(func)
+            return self.base_rate(func, thresh=thd)
 
-    def base_metrics(self, metrics="FPR", privileged=None):
+    def general_group_fairness_metrics(self, metrics="DP"):
+        # the metrics are generalized when threshold is set to None
+        return self.group_fairness_metrics(metrics=metrics, thd=None)
+
+    def base_metrics(self, metrics="FPR", privileged=None, thd=0.5):
         if metrics not in list(ModelMetrics.BASE_METRICS.keys()):
             raise ValueError(f"Metrices \'{metrics}\' not supported.")
         func = ModelMetrics.BASE_METRICS[metrics]
-        return self.base_rate(func, privileged)
+        return self.base_rate(func, privileged, thresh=thd)
+
+    def general_base_metrics(self, metrics="FPR", privileged=None):
+        # the metrics are generalized when threshold is set to None
+        return self.base_metrics(metrics=metrics, privileged=privileged, thd=None)
 
 
-    def base_rate(self,  func, favorable=None):
+    def base_rate(self,  func, favorable=None, thresh=0.5):
         mask = None
         result = {}
         if favorable is None:
@@ -103,69 +148,34 @@ class ModelMetrics(DatasetMetrics): # binary label and binary group only
         if len(self.sensitive) == 1:
             i  = list(self.dataset.privileged.keys()).index(self.sensitive[0])
             if func.__code__.co_argcount > 4:
-                return func(self.yh, self.y, self.z[:, i], self.w, self.thresh)
+                return func(self.yh, self.y, self.z[:, i], self.w, thresh)
             y_z = self.y[mask[:, i]]
             yh_z = self.yh[mask[:, i]]
             w_z = self.w[mask[:, i]]
             # z_z = self.z[mask[:, i]][:, i]
-            return func(yh_z, y_z, w_z, self.thresh)
+            return func(yh_z, y_z, w_z, thresh)
         
         # go through sensitive attributes
         for (i, attr) in enumerate(self.sensitive):
             if favorable is None: # for none single group metrics
-                result[attr] = func(self.yh, self.y, self.z[:, i], self.w, self.thresh)
+                result[attr] = func(self.yh, self.y, self.z[:, i], self.w, thresh)
                 continue
             y_z = self.y[mask[:, i]]
             yh_z = self.yh[mask[:, i]]
             w_z = self.w[mask[:, i]]
             # z_z = self.z[mask[:, i]][:, i]
-            result[attr] = func(yh_z, y_z, w_z, self.thresh)
+            result[attr] = func(yh_z, y_z, w_z, thresh)
         
         return result
 
-    def diff(self, func):
-        return dic_operation(self.base_rate(func, True), self.base_rate(func, False), lambda x, y: x - y)
+    def diff(self, func, **kwargs):
+        return dic_operation(self.base_rate(func, True, **kwargs), self.base_rate(func, False, **kwargs), lambda x, y: abs(x - y))
 
-    def ratio(self, func):
-        return dic_operation(self.base_rate(func, False), self.base_rate(func, True), lambda x, y: x / (y+1e-6))
+    def ratio(self, func, **kwargs):
+        return dic_operation(self.base_rate(func, False, **kwargs), self.base_rate(func, True, **kwargs), lambda x, y: x / (y+1e-6))
 
     def consistency(self, n_neighbors=5):
-        r"""Individual fairness metric from [1]_ that measures how similar the
-        labels are for similar instances.
 
-        .. math::
-            1 - \frac{1}{n\cdot\text{n_neighbors}}\sum_{i=1}^n |\hat{y}_i -
-            \sum_{j\in\mathcal{N}_{\text{n_neighbors}}(x_i)} \hat{y}_j|
-
-        Args:
-            n_neighbors (int, optional): Number of neighbors for the knn
-                computation.
-
-        References:
-            .. [1] R. Zemel, Y. Wu, K. Swersky, T. Pitassi, and C. Dwork,
-                "Learning Fair Representations,"
-                International Conference on Machine Learning, 2013.
-        """
-
-        X = np.array(self.dataset.X)
-        num_samples = X.shape[0]
+        x = np.array(self.dataset.X)
         y = self.yh
-
-        # learn a KNN on the features
-        nbrs = NearestNeighbors(n_neighbors=n_neighbors, algorithm='ball_tree').fit(X)
-        _, indices = nbrs.kneighbors(X)
-
-        # compute consistency score
-        consistency = 0.0
-        for i in range(num_samples):
-            consistency += np.abs(y[i] - np.mean(y[indices[i]])) * self.w[i]
-        consistency = 1.0 - consistency/num_samples
-
-        return consistency
-        
-
-
-
-    
-
-    
+        return consistency(x, y)
