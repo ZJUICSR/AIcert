f6b265eb8582a727ba69e0ca1d955d2968f87f26
chunlai <1017313484@qq.com>
2023-05-03 16:03:09 +0800

    修改bug

--- function/attack/estimators/keras.py
+++ function/attack/estimators/keras.py
@@ -18,68 +18,68 @@
 # """
 # This module implements the abstract estimator `KerasEstimator` for Keras models.
 # """
-# from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-# import logging
+import logging
 
-# import numpy as np
+import numpy as np
 
-# from function.attack.estimators.estimator import (
-#     BaseEstimator,
-#     NeuralNetworkMixin,
-#     LossGradientsMixin,
-# )
+from function.attack.estimators.estimator import (
+    BaseEstimator,
+    NeuralNetworkMixin,
+    LossGradientsMixin,
+)
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
-# class KerasEstimator(NeuralNetworkMixin, LossGradientsMixin, BaseEstimator):
-#     """
-#     Estimator class for Keras models.
-#     """
+class KerasEstimator(NeuralNetworkMixin, LossGradientsMixin, BaseEstimator):
+    """
+    Estimator class for Keras models.
+    """
 
-#     estimator_params = BaseEstimator.estimator_params + NeuralNetworkMixin.estimator_params
+    estimator_params = BaseEstimator.estimator_params + NeuralNetworkMixin.estimator_params
 
-#     def __init__(self, **kwargs) -> None:
-#         """
-#         Estimator class for Keras models.
-#         """
-#         super().__init__(**kwargs)
+    def __init__(self, **kwargs) -> None:
+        """
+        Estimator class for Keras models.
+        """
+        super().__init__(**kwargs)
 
-#     def predict(self, x: np.ndarray, batch_size: int = 128, **kwargs):
-#         """
-#         Perform prediction of the neural network for samples `x`.
+    def predict(self, x: np.ndarray, batch_size: int = 128, **kwargs):
+        """
+        Perform prediction of the neural network for samples `x`.
 
-#         :param x: Samples of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
-#                   nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2).
-#         :param batch_size: Batch size.
-#         :return: Predictions.
-#         :rtype: Format as expected by the `model`
-#         """
-#         return NeuralNetworkMixin.predict(self, x, batch_size=batch_size, **kwargs)
+        :param x: Samples of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
+                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2).
+        :param batch_size: Batch size.
+        :return: Predictions.
+        :rtype: Format as expected by the `model`
+        """
+        return NeuralNetworkMixin.predict(self, x, batch_size=batch_size, **kwargs)
 
-#     def fit(self, x: np.ndarray, y, batch_size: int = 128, nb_epochs: int = 20, **kwargs) -> None:
-#         """
-#         Fit the model of the estimator on the training data `x` and `y`.
+    def fit(self, x: np.ndarray, y, batch_size: int = 128, nb_epochs: int = 20, **kwargs) -> None:
+        """
+        Fit the model of the estimator on the training data `x` and `y`.
 
-#         :param x: Samples of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
-#                   nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2).
-#         :param y: Target values.
-#         :type y: Format as expected by the `model`
-#         :param batch_size: Batch size.
-#         :param nb_epochs: Number of training epochs.
-#         """
-#         NeuralNetworkMixin.fit(self, x, y, batch_size=batch_size, nb_epochs=nb_epochs, **kwargs)
+        :param x: Samples of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
+                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2).
+        :param y: Target values.
+        :type y: Format as expected by the `model`
+        :param batch_size: Batch size.
+        :param nb_epochs: Number of training epochs.
+        """
+        NeuralNetworkMixin.fit(self, x, y, batch_size=batch_size, nb_epochs=nb_epochs, **kwargs)
 
-#     def compute_loss(self, x: np.ndarray, y: np.ndarray, **kwargs) -> np.ndarray:
-#         """
-#         Compute the loss of the neural network for samples `x`.
+    def compute_loss(self, x: np.ndarray, y: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Compute the loss of the neural network for samples `x`.
 
-#         :param x: Samples of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
-#                   nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2).
-#         :param y: Target values (class labels) one-hot-encoded of shape `(nb_samples, nb_classes)` or indices
-#                   of shape `(nb_samples,)`.
-#         :return: Loss values.
-#         :rtype: Format as expected by the `model`
-#         """
-#         raise NotImplementedError
+        :param x: Samples of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
+                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2).
+        :param y: Target values (class labels) one-hot-encoded of shape `(nb_samples, nb_classes)` or indices
+                  of shape `(nb_samples,)`.
+        :return: Loss values.
+        :rtype: Format as expected by the `model`
+        """
+        raise NotImplementedError
