0bddec1b5de83880ee89166d8a68cf79df3b7b06
signedqiu <10063108+signedqiu@user.noreply.gitee.com>
2023-05-20 01:03:45 +0800

    change the interface in backdoor attack

--- test.py
+++ test.py
@@ -76,31 +76,40 @@ from function.attack.attacks.utils import load_mnist, load_cifar10
 # a.generate(method="SquareAttack", norm="inf", save_num=32, eps=0.156, p_init=0.03, loss_type="margin", n_restarts=3)
 # a.print_res()
 
+
 # 后门攻击测试
 ## MNIST
 # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# b = BackdoorAttacker(modelnet=ResNet18(1), modelpath="./model/model_ckpt/ckpt-resnet18-mnist_epoch3_acc0.9898.pth", 
+# b = BackdoorAttacker(modelnet=ResNet18(1), modelpath="./model/model_ckpt/resnet_mnist_random.pth", 
 #     dataset="mnist", datasetpath="./datasets/", nb_classes=10, datanormalize = False, 
 #     device=device)
 ### PoisoningAttackBackdoor
-# b.backdoorattack(method="PoisoningAttackBackdoor",batch_size=128, pp_poison=0.01, target=1, test_sample_num=1024)
+# b.poision(method="PoisoningAttackAdversarialEmbedding", trigger=[16,16,3,1], pp_poison=0.01)
+# b.train()
 ### PoisoningAttackCleanLabelBackdoor
-# b.backdoorattack(method="PoisoningAttackCleanLabelBackdoor", batch_size=128, pp_poison=0.01, target=1, test_sample_num=1024, eps=0.3)
+# b.poision(method="PoisoningAttackAdversarialEmbedding", trigger=[16,16,3,1], pp_poison=0.01)
+# b.train()
 ### FeatureCollisionAttack
-# b.backdoorattack(method="FeatureCollisionAttack", batch_size=128, pp_poison=0.01, target=1, test_sample_num=1024)
+# b.poision(method="PoisoningAttackAdversarialEmbedding", trigger=[16,16,3,1], pp_poison=0.01)
+# b.train()
 ### PoisoningAttackAdversarialEmbedding
-# b.backdoorattack(method="PoisoningAttackAdversarialEmbedding", batch_size=128, pp_poison=0.01, target=1, test_sample_num=1024)
+# b.poision(method="PoisoningAttackAdversarialEmbedding", trigger=[16,16,3,1], pp_poison=0.01)
+# b.train()
 
 ## CIFAR10
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-b = BackdoorAttacker(modelnet=ResNet18(3), modelpath="./model/model_ckpt/ckpt-resnet18-cifar10_epoch19_acc0.8335.pth", 
+b = BackdoorAttacker(modelnet=ResNet18(3), modelpath="./model/model_ckpt/resnet_cifar10_random.pth", 
     dataset="cifar10", datasetpath="./datasets/", nb_classes=10, datanormalize = False, 
     device=device)
 ### PoisoningAttackBackdoor
-# b.backdoorattack(method="PoisoningAttackBackdoor",batch_size=128, pp_poison=0.01, target=1, test_sample_num=1024)
+# b.poision(method="PoisoningAttackBackdoor", trigger=[16,16,3,1], pp_poison=0.01, save_num=32, test_sample_num=4096, source=0, target=3)
+# b.train(num_epochs=20, batch_size=128, save_model=True)
 ### PoisoningAttackCleanLabelBackdoor
-# b.backdoorattack(method="PoisoningAttackCleanLabelBackdoor", batch_size=128, pp_poison=0.01, target=1, test_sample_num=1024)
+# b.poision(method="PoisoningAttackCleanLabelBackdoor", trigger=[16,16,3,1], pp_poison=0.01)
+# b.train()
 ### FeatureCollisionAttack
-b.backdoorattack(method="FeatureCollisionAttack", batch_size=128, pp_poison=0.1, target=1, test_sample_num=1024)
+# b.poision(method="FeatureCollisionAttack", trigger=[16,16,3,1], pp_poison=0.01)
+# b.train()
 ### PoisoningAttackAdversarialEmbedding
-# b.backdoorattack(method="PoisoningAttackAdversarialEmbedding", batch_size=128, pp_poison=0.01, target=1, test_sample_num=1024)
+b.poision(method="PoisoningAttackAdversarialEmbedding", trigger=[16,16,3,1], pp_poison=0.01)
+b.train()
