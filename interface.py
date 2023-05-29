f15b0ca54197743499389349882fd5cf76c2b84a
NiuWenjie <winnie_niuwj@163.com>
2023-05-29 10:06:58 +0800

    debug

--- interface.py
+++ interface.py
@@ -204,7 +204,8 @@ def run_concolic(tid, AAtid, dataname, modelname, norm, times):
     :params norm:范数约束
     """
     taskinfo = IOtool.load_json(osp.join(ROOT,"output","task_info.json"))
-    res = concolic.run_concolic(dataname.lower(), modelname.lower(), norm.lower(), int(times), osp.join(ROOT,"output", tid, AAtid))   
+    res = concolic.run_concolic(dataname.lower(), modelname.lower(), norm.lower(), int(times), osp.join(ROOT,"output", tid, AAtid))  
+    res["stop"]=1 
     IOtool.write_json(res,osp.join(ROOT,"output", tid, AAtid+"_result.json"))
     taskinfo[tid]["function"][AAtid]["state"]=2
     taskinfo[tid]["state"]=2
