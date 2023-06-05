9296345b43685d8bee272ab2868cfbc46ab58b5b
shybeejd <shybeejd@gmail.com>
2023-06-05 07:07:30 +0000

    add verify

---
+++ function/formal_verify/knowledge_consistency/Model_zoo/__init__.py
@@ -0,0 +1,7 @@
+from .VGG_ImageNet import *
+from .ResNets import *
+from .linearTest_sigmoidP_instance import *
+from . import VGG_MNIST
+#models = {}
+#models["vgg_bn_ft"] = vgg_bn_ft
+#models["alexnet_ft"] = alexnet_ft
