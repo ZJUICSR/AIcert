191b4d3b9f1dd1b0213c394fa7880b6c9a06ef03
NiuWenjie <winnie_niuwj@163.com>
2023-06-06 09:15:12 +0800

    converage debug

--- function/coverage/__init__.py
+++ function/coverage/__init__.py
@@ -1,5 +1,5 @@
 # from coverage_layer import * # 以此为执行脚本时使用
-from .coverage_layer import *
+# from .coverage_layer import *
 # from coverage_neural import *
 from .coverage_neural import *
 import os.path as osp
@@ -13,8 +13,10 @@ def run_coverage_neural_func(dataset, model, k, N, out_path, logging):
     
     model_path: 模型路径
     """
-    model_path = CURR.rsplit('/',2)[0]+"/model/ckpt/lenet5.pth"
+    model_path = CURR.rsplit('/',2)[0]+"/model/ckpt/lenet5_dict.pth"
+    # res = run_visualize_neural(dataset=dataset, model_type=model, k=0.1, model_path=model_path, outputdir=out_path, logging=logging,number_of_image=N)
     res = run_visualize_neural(dataset=dataset, model_type=model, k=0.1, model_path=model_path, outputdir=out_path, logging=logging,number_of_image=N)
+
     return res
 
 def run_coverage_layer_func(dataset, model, k, N, out_path, logging):
@@ -29,8 +31,9 @@ def run_coverage_layer_func(dataset, model, k, N, out_path, logging):
     return res
 
 if __name__ == "__main__":
-    model_path = CURR.rsplit('/',2)[0]+"/model/ckpt/lenet5.pth"
-    # res = run_visualize_neural(dataset="mnist", model_type="lenet5", k=0.1, model_path=model_path, number_of_image=100)
+    res = run_coverage_layer_func
+    model_path = CURR.rsplit('/',2)[0]+"/model/ckpt/vgg11_dict.pth"
+    res = run_visualize_neural(dataset="cifar10", model_type="vgg11", k=0.1, model_path=model_path, number_of_image=100)
     # print(res)
-    res = run_visualize_layer(dataset="mnist", model_type="lenet5", k=0.1, model_path=model_path, number_of_image=100)
+    # res = run_visualize_layer(dataset="mnist", model_type="lenet5", k=0.1, model_path=model_path, number_of_image=100)
     print(res)
