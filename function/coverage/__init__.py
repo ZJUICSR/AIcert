b0c6ad6532b6a49f44c1ece3d200eb0c8a745b33
NiuWenjie <winnie_niuwj@163.com>
2023-06-08 09:40:47 +0800

    重要神经元f覆盖测试debug

--- function/coverage/__init__.py
+++ function/coverage/__init__.py
@@ -1,7 +1,8 @@
 # from coverage_layer import * # 以此为执行脚本时使用
-from .coverage_layer import *
 # from coverage_neural import *
+from .coverage_layer import *
 from .coverage_neural import *
+from .coverage_importance import *
 import os.path as osp
 CURR = osp.dirname(osp.abspath(__file__))
 
@@ -30,6 +31,20 @@ def run_coverage_layer_func(dataset, model, k, N, out_path, logging):
     logging.info("Starting running......")
     model_path = CURR.rsplit('/',2)[0]+"/model/ckpt/lenet5_dict.pth"
     res = run_visualize_layer(dataset=dataset, model_type=model, k=k, model_path=model_path, outputdir=out_path, logging=logging,number_of_image=N)
+    logging.info("Finishing......")
+    return res
+
+def run_coverage_importance_func(dataset, model, n_imp, clus, out_path, logging):
+    """
+    dataset: 数据集名称
+    model: 模型名称
+    n_imp: 重要神经元数目
+    clus: 聚类数
+    model_path: 模型路径
+    """
+    logging.info("Starting running......")
+    model_path = CURR.rsplit('/',2)[0]+"/model/ckpt/model.pth"
+    res = run_visualize_importance(dataset=dataset, model_type=model, n_imp=n_imp, clus=clus, model_path=model_path, out_path=out_path, logging=logging)
     logging.info("Finishing......")
     return res
 
