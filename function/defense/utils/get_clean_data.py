1a5f76b45eda7d55b109def591a911642c6cfe3a
x1a0yue <1926340354@qq.com>
2023-06-14 08:29:22 +0000

    add CARTL and SAGE

--- function/defense/utils/get_clean_data.py
+++ function/defense/utils/get_clean_data.py
@@ -6,14 +6,18 @@ from torchvision import datasets
 # from .imagenette import ImageNette
 import torchvision.transforms as transforms
 
-def get_clean_loader(dataset):
+def get_clean_loader(dataset, normalize=False):
     if dataset == 'CIFAR10':
         num_classes = 10
-        tf_test = transforms.Compose([transforms.ToTensor(),
-                                #   transforms.Normalize(
-                                #     mean=[0.4914, 0.4822, 0.4465],
-                                #     std=[0.2023, 0.1994, 0.2010],)
-                                ])
+        if normalize:
+            tf_test = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                    mean=[0.49139765, 0.48215759, 0.44653141], #[0.4914, 0.4822, 0.4465]
+                    std=[0.24703199, 0.24348481, 0.26158789],)]) #[0.2023, 0.1994, 0.2010]
+        else: 
+            tf_test = transforms.Compose([
+                transforms.ToTensor(),])
         cleanset = datasets.CIFAR10(
             root='/mnt/data2/yxl/AI-platform/dataset/CIFAR10', train=False, download=True, transform=tf_test)
         clean_loader = DataLoader(dataset=cleanset,
@@ -22,10 +26,13 @@ def get_clean_loader(dataset):
                                    )
     elif dataset == 'MNIST':
         num_classes = 10
-        tf_test = transforms.Compose([
+        if normalize:
+            tf_test = transforms.Compose([
                     transforms.ToTensor(),
-                    # transforms.Normalize((0.1307, ), (0.3081, ))
-            ])
+                    transforms.Normalize((0.1307, ), (0.3081, ))])
+        else:
+            tf_test = transforms.Compose([
+                    transforms.ToTensor(),])
         cleanset = datasets.MNIST(
             root = '/mnt/data2/yxl/AI-platform/dataset', train=False, download=True, transform=tf_test
         )
