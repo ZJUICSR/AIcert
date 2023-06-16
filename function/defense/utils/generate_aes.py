9e17ef91563a7e8671d66d358140ad6632e3e912
x1a0yue <1926340354@qq.com>
2023-06-16 11:26:42 +0000

    add output adversarial images

--- function/defense/utils/generate_aes.py
+++ function/defense/utils/generate_aes.py
@@ -1,8 +1,19 @@
+import os
 import torch
 import numpy as np
-import torch.nn as nn
+from PIL import Image
 from function.defense.utils.get_clean_data import get_clean_loader
 from advertorch.attacks import GradientSignAttack, MomentumIterativeAttack, SparseL1DescentAttack, CarliniWagnerL2Attack, LinfPGDAttack
+
+def save_jepg(adv_examples, output_path, count):
+    # 将torch加载的图像转换为PIL图像
+    image = adv_examples.cpu().numpy().transpose(1, 2, 0)
+    image_uint8 = (image * 255).astype(np.uint8)
+    pil_image = Image.fromarray(image_uint8)
+
+    # 保存为JPEG图像
+    pil_image.save(output_path, "JPEG")
+
 def generate_adv_examples(model, adv_method, adv_dataset, adv_nums, device, normalize=False):
     clean_loader, num_classes = get_clean_loader(adv_dataset, normalize)
     if adv_dataset == 'CIFAR10':
@@ -65,5 +76,20 @@ def generate_adv_examples(model, adv_method, adv_dataset, adv_nums, device, norm
             break
     # adv_examples = adversary.perturb(clean_examples, true_labels)
     adv_examples = adv_examples[:adv_nums]
+    count = 0
+    image_num = min(adv_nums, 10)
+    for i in range(image_num):
+        output_dir = "/mnt/data2/yxl/AI-platform/output/images/" + str(i)
+        if not os.path.exists(output_dir):
+            # 如果文件夹不存在，则创建文件夹
+            os.makedirs(output_dir)
+        save_jepg(adv_examples[i], output_dir + '/adv.jpeg', count)
+        save_jepg(clean_examples[i], output_dir + '/clean.jpeg', count)
+        save_jepg(adv_examples[i] - clean_examples[i], output_dir + '/noise.jpeg', count)
     # np.save('/mnt/data2/yxl/AI-platform/adv_' + adv_dataset + '_' + adv_method + '.npy', adv_examples.cpu().numpy())
+    # torch.save({
+    #     'adv_img': adv_examples,
+    #     'cln_img': clean_examples,
+    #     'y': true_labels
+    # }, '/mnt/data2/yxl/AI-platform/dataset/adv_' + adv_dataset + '_' + adv_method + '.pt')
     return adv_examples, clean_examples, true_labels
