f174d06c319682d687747dc3639cd622a7635eab
x1a0yue <1926340354@qq.com>
2023-06-16 09:14:14 +0000

    add upload adversarial examples

--- function/defense/region_based.py
+++ function/defense/region_based.py
@@ -45,14 +45,8 @@ class RegionBased(object):
 
     def load_adv_examples(self):
         data = torch.load(self.adv_examples)
-        adv_dst = TensorDataset(data["x"].float().cpu(), data["y"].long().cpu())
-        adv_loader = DataLoader(
-        adv_dst,
-        batch_size=1,
-        shuffle=False,
-        num_workers=2
-        )
-        return adv_loader
+        print('successfully load adversarial examples!')
+        return data['adv_img'], data['cln_img'], data['y']
 
 
     def region_based(self, img):
@@ -129,7 +123,7 @@ class RegionBased(object):
         if self.adv_examples is None:
             adv_imgs, clean_imgs, true_labels = self.generate_adv_examples()
         else:
-            adv_imgs, adv_labels = self.load_adv_examples()
+            adv_imgs, clean_imgs, true_labels = self.load_adv_examples()
 
         for i in range(len(adv_imgs)):
             self.total_num += 1
