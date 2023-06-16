ae9b6b0bc8d4ffb2dc1c913878013ad166074459
笙声不息 <775895229@qq.com>
2023-06-16 12:45:43 +0000

    更新形式化验证接口
    Signed-off-by: 笙声不息 <775895229@qq.com>

--- function/formal_verify/__init__.py
+++ function/formal_verify/__init__.py
@@ -8,5 +8,7 @@ from auto_LiRPA_verifiy.vision.verify import verify as vision_verify
 from auto_LiRPA_verifiy.language.verify import verify as language_verify
 from auto_LiRPA_verifiy.vision.cnn import mnist_model
 from auto_LiRPA_verifiy.vision.data import get_mnist_data, get_cifar_data, get_gtsrb_data, get_MTFL_data
-from auto_LiRPA_verifiy.vision.model import get_mnist_cnn_model, get_cifar_resnet18, get_gtsrb_resnet18, get_MTFL_resnet18
+from auto_LiRPA_verifiy.vision.model import get_mnist_cnn_model, get_cifar_resnet18, get_gtsrb_resnet18, get_MTFL_resnet18, get_cifar_densenet_model
+from auto_LiRPA_verifiy.language.Transformer.Transformer import get_transformer_model
+from auto_LiRPA_verifiy.language.lstm import get_lstm_demo_model
 
