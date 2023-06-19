3b341ef7fa30248f017ff05105fa393cee1e02a2
chunlai <1017313484@qq.com>
2023-06-19 09:34:56 +0800

    修改bug

--- function/formal_verify/auto_verify/__init__.py
+++ function/formal_verify/auto_verify/__init__.py
@@ -61,7 +61,6 @@ def auto_verify_img(net,data,eps,input_file='./test.jpg',device='cuda'):
     else:
         print(model)
         model_ori = Models[model](in_ch=3, in_dim=32)
-    
     checkpoint = torch.load(load)
     epoch, state_dict = checkpoint['epoch'], checkpoint['state_dict']
     opt_state = None
