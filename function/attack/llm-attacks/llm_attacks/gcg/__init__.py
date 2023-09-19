ed53104918ae1943716591c1fb1b3a4acae99d7a
AIsecurity <1017313484@qq.com>
2023-09-19 03:46:15 +0000

    Revert "fix reach bug"
    This reverts commit d6f52aa415f80fd7729df9df67a5235f26ef7158.

--- function/attack/llm-attacks/llm_attacks/gcg/__init__.py
+++
@@ -1,6 +0,0 @@
-from .gcg_attack import GCGAttackPrompt as AttackPrompt
-from .gcg_attack import GCGPromptManager as PromptManager
-from .gcg_attack import GCGMultiPromptAttack as MultiPromptAttack
-
-from llm_attacks import ProgressiveMultiPromptAttack
-from llm_attacks import IndividualPromptAttack
