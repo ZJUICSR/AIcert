f8b959fb5c4988c1b7f6e50a1f0fc0d9a683c374
chunlai <1017313484@qq.com>
2023-11-07 16:58:59 +0800

    fix bug

--- function/fairness/tabular/debias/inprocess/__init__.py
+++ function/fairness/tabular/debias/inprocess/__init__.py
@@ -3,6 +3,6 @@ from function.fairness.tabular.debias.inprocess.adversarial_debiasing import FAD
 from function.fairness.tabular.debias.inprocess.domain_indipendent import DomainIndependentClassifier
 from function.fairness.tabular.debias.inprocess.ensemble import EnsembleClassifier
 from function.fairness.tabular.debias.inprocess.fair_mixup import FMClassifier
-# from function.fairness.tabular.debias.inprocess.CFair import CFairClassifier
+from function.fairness.tabular.debias.inprocess.CFair import CFairClassifier
 
 
