5a1f23728467e75b0600ac2e503a7f0bb5bd11ef
chunlai <1017313484@qq.com>
2023-11-27 17:09:03 +0800

    update

--- function/attack/__init__.py
+++ function/attack/__init__.py
@@ -2,6 +2,11 @@ from function.attack.attacks import *
 from function.attack.estimators import *
 from function.attack import attack_api, train_network
 from model.model_net.resnet_attack import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
+from model.model_net.resnet_attack_feature import ResNet18 as ResNet18_f
+from model.model_net.resnet_attack_feature import ResNet34 as ResNet34_f
+from model.model_net.resnet_attack_feature import ResNet50 as ResNet50_f
+from model.model_net.resnet_attack_feature import ResNet101 as ResNet101_f
+from model.model_net.resnet_attack_feature import ResNet152 as ResNet152_f
 from function.attack.attack_api import EvasionAttacker, BackdoorAttacker
 import torch, os, time
 import os.path as osp
@@ -182,12 +187,14 @@ class Attack_Obj(object):
         attack_info = self.AttackObj.get_adv_data(dataloader=self.dataloader, method=self.method_in_alg, **self.attackparam)
         return attack_info
     
-
-
-def run_backdoor(model, modelpath, dataname, method, pp_poison, save_num, test_sample_num, target, trigger, device, nb_classes=10, method_param=None):
+def run_backdoor(model, modelpath, dataname, method, pp_poison, save_num,  target, trigger, device, 
+                 test_sample_num=1000, train_sample_num=1000, nb_classes=10, method_param=None, feature=False):
+    model = model+'_f' if feature else model
     datasetpath="./datasets/"
     channel = 1 if dataname.lower() == "mnist" else 3
-    b = BackdoorAttacker(modelnet=eval(model)(channel), modelpath=modelpath, dataset=dataname.lower(), datasetpath=datasetpath, nb_classes=nb_classes, datanormalize=False, device=torch.device(device))
+    b = BackdoorAttacker(modelnet=eval(model)(channel), modelpath=modelpath, dataset=dataname.lower(), datasetpath=datasetpath, 
+                         nb_classes=nb_classes, datanormalize=False, device=torch.device(device),
+                         test_sample_num=test_sample_num, train_sample_num=train_sample_num)
     # 对应关系list
     methoddict={
         "PoisoningAttackBackdoor":"PoisoningAttackBackdoor",
@@ -210,15 +217,28 @@ def run_backdoor(model, modelpath, dataname, method, pp_poison, save_num, test_s
     #           target=target, trigger=trigger, **method_param)
     b.poision(method=methoddict[method], **method_param)
     res["accuracy"], res["accuracyonb"], res["attack_success_rate"] = b.train()
-    res["accuracy"] = round(res["accuracy"],1)
-    res["accuracyonb"] = round(res["accuracyonb"],1)
-    res["attack_success_rate"] = round(res["attack_success_rate"],1)
+    res["accuracy"] = round(res["accuracy"],3)
+    res["accuracyonb"] = round(res["accuracyonb"],3)
+    res["attack_success_rate"] = round(res["attack_success_rate"],3)
+    pp_ = method_param['pp_poison']
+    save_num_ = method_param['save_num']
+    method_param['save_num'] = 0
     for i in range(5):
-        temp_pp_poison = 0.001 + 0.1 * i
+        if i == 0:
+            temp_pp_poison = 0.001
+        else:
+            temp_pp_poison = 0.01 * i
+        method_param['pp_poison'] = temp_pp_poison
+        
         # b.poision(method=methoddict[method], pp_poison=temp_pp_poison, test_sample_num=test_sample_num, target=target, trigger=trigger, **method_param)
-        b.poision(method=methoddict[method], **method_param)
-        accuracy, accuracyonb, attack_success_rate = b.train()
-        res["asr"].append(round(attack_success_rate*100,1))
+        code = b.poision(method=methoddict[method], **method_param)
+        if code == -1:
+            res["asr"].append(0)
+        else:
+            accuracy, accuracyonb, attack_success_rate = b.train()
+            res["asr"].append(round(attack_success_rate*100,1))
         res["pp_poison"].append(temp_pp_poison)
+    method_param['save_num'] = save_num_
+    method_param['pp_poison'] = pp_
     return res
 
